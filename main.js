217d99b382f90d3e6fb0467c798ff0f138f1cafd
Siddharth <siddharthg@valuefy.com>
2019-07-03 10:04:42 +0530

    single service based logic

--- main.js
+++ main.js
@@ -3,35 +3,19 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const cors = require('cors');
 const Jimp = require('jimp');
-const fs = require('fs');
-const path = require('path');
-const mkdirp = require('mkdirp');
 
 const PORT = process.env.PORT || 3000;
-const UPLOADDIR = 'uploads';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const dir = path.resolve(__dirname, UPLOADDIR);
-    console.log({ dir });
-    mkdirp(dir, err => cb(err, dir));
-  },
-  filename: function (req, file, cb) {
-    cb(null, "" + Date.now())
-  }
-})
+
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage })
 
-
-app.get('/ping', (req, res) => {
-  res.send("Ping Successful");
-});
 
 
 app.post('/uploadfile', upload.single('imageFile'), (req, res, next) => {
@@ -43,21 +27,27 @@ app.post('/uploadfile', upload.single('imageFile'), (req, res, next) => {
     });
   }
 
-  const filename = file.path + '.jpg';
-
-  Jimp.read(file.path)
+  Jimp.read(file.buffer)
     .then(fileInstance => {
 
       fileInstance
         .brightness(0.18)
         .contrast(-0.18)
         .color([{ apply: 'lighten', params: [3] }])
-        .write(filename);
+        .getBase64(Jimp.MIME_JPEG, (err, dataString) => {
 
-      return res.send({
-        success: true,
-        file: filename
-      });
+          if (err) {
+            res.status(err.status).send({
+              message: err
+            });
+          }
+
+          return res.send({
+            success: true,
+            dataURL: dataString,
+            fileName: Date.now() + '-washed.jpg'
+          });
+        });
 
     })
     .catch(err => {
@@ -67,43 +57,6 @@ app.post('/uploadfile', upload.single('imageFile'), (req, res, next) => {
       });
     })
 });
-
-app.get('/download', (req, res) => {
-
-  if (!req.query.filename) {
-    return res.end();
-  }
-
-  const filePath = path.resolve(__dirname, req.query.filename);
-  const fileNameForSave = 'washed' + Date.now() + '.jpg';
-
-  return res.download(filePath, fileNameForSave, (err) => {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-
-    deleteAllUploads()
-
-  })
-
-
-});
-
-
-const deleteAllUploads = () => {
-  const dir = path.resolve(__dirname, UPLOADDIR);
-  fs.readdir(dir, (err, files) => {
-    if (err) throw err;
-
-    for (const file of files) {
-      fs.unlink(path.join(dir, file), err => {
-        if (err) throw err;
-      });
-    }
-  })
-}
-
 
 app.listen(PORT, () => {
   console.log("Listening on: ", PORT);
